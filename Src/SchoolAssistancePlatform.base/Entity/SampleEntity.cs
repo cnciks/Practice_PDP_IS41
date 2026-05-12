d1782de68bef60f55368d557b10f1d7a09941f2a
Stankevich <iviramor.di@gmail.com>
2026-05-12 21:37:36 +0300

    added src

---
+++ Src/SchoolAssistancePlatform.base/Entity/SampleEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SchoolAssistancePlatform.Base.Entity;
+
+[Table("Sample", Schema = "School")]
+public class SampleEntity
+{
+	/// <summary> Идентификатор. </summary>
+	[Key]
+	public long Id { get; set; }
+
+
+}
